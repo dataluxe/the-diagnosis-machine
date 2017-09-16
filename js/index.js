300615d6e27dd37254a1c75295a42b32b87d1281
Nonya Biznes <nonya@biznes.com>
2017-09-16 14:31:35 -0700

    Added all JS (first draft) for index.html, added sample bulk-definitions to massDefinitions.js.
    Small changes to Design Rough.txt, added script tag, minor changes to index.html.

--- js/index.js
+++ js/index.js
@@ -10,91 +10,107 @@ function User (name, password) {
     this.checkoutTotal = null;
 }
 
-var userIndex = []; /* Replace with localStorage check */
+var userIndex = (localStorage.getItem("userIndex") === null ? [] : JSON.parse(localStorage.getItem("userIndex")));
+var currentUser;
+var properRegex = /^[A-Za-z0-9]{1,20}$/;
 
-var currentUser = null;
+/* Sign Up function here */
 
-const symptomIndex = [
-    ["Energy", "../images/symptoms/energy.png"],
-    ["Digestion", "../images/symptoms/digestion.png"],
-    ["Mood", "../images/symptoms/mood.png"],
-    ["Headaches", "../images/symptoms/headaches.png"],
-    ["Foggy Brain", "../images/symptoms/foggybrain.png"],
-    ["Bone Problems", "../images/symptoms/boneproblems.png"],
-    ["Allergies", "../images/symptoms/allergies.png"],
-    ["Sugar Cravings", "../images/symptoms/sugercravings.png"],
-    ["Heart Trouble", "../images/symptoms/hearttrouble.png"],
-    ["Recurring Flu / Colds", "../images/symptoms/recurringflu.png"],
-    ["Romantic Health", "../images/symptoms/romantichealth.png"],
-    ["General Nutrition", "../images/symptoms/generalnutrition.png"]
-];
+const signUp = function() {
+    var unField = document.getElementById("username-field");
+    var pwField = document.getElementById("password-field");
+    var un = unField.value;
+    var pw = pwField.value;
+    var regexPass = (properRegex.test(un) && properRegex.test(pw));
+    var unCheck = (function(){ /*function returns true if entered username is found, false if not. */
+        for (var i = 0; i < userIndex.length; i++) {
+            if (userIndex[i].name === un) {
+                return true;
+            } else {}
+        }
+        return false;
+    })();
 
-function Supplement (inpObj) {
-    this.name = inpObj.name; //string
-    this.link = inpObj.link; //string - external link
-    this.picture = "./images/supplements/" + inpObj.name + ".jpg"; //image filename must equal supplement name
-    this.description = inpObj.description; //string. Good reason to make these in external blocks
-    this.price = inpObj.price;
-    this.symptoms = inpObj.symptoms;
-}
+    if (!regexPass)
+    {
+        unField.value = "";
+        pwField.value = "";
+        window.alert("We cannot accept that username and/or password.\n\nPlease enter a string of letters ( [aA-zZ] and numbers [0-9] up to 20 characters long ) in both fields!");
+    }
 
-var supplementIndex = [];
+    else if ( regexPass && unCheck) {
+        window.alert("User already exists! Please either Sign In as that user, or enter a different username.");
+    }
 
-const calciumComplex = {
-    name : "Calcium Complex",
-    link : "",
-    description : "",
-    price: 29.95,
-    symptoms: {
-        "Energy" : 5,
-        "Digestion" : 5,
-        "Mood" : 2,
-        "Headaches" : 5,
-        "Foggy Brain" : 0,
-        "Bone Problems" : 3,
-        "Allergies" : 5,
-        "Sugar Cravings" : 7,
-        "Heart Trouble" : 8,
-        "Recurring Flu / Colds" : 4,
-        "Romantic Health" : 4,
-        "General Nutrition": 6
+    else if ( regexPass && !unCheck ) {
+        currentUser = new User(un, pw);
+        localStorage.setItem("currentUser", JSON.stringify(currentUser));
+        userIndex.push(currentUser);
+        localStorage.setItem("userIndex", JSON.stringify(userIndex));
+        console.log("User Created, pushed to Array: " + currentUser.name + " | " + currentUser.password + "/n" +  userIndex);
+        //window.location="symptoms.html"
+    }
+
+    else {
+        console.log ("!!! CRITICAL: Unhandled 'if' case in function 'signUp()'. Investigate and repair!");
     }
 };
 
-supplementIndex.push(new Supplement(calciumComplex));
+/* Sign In function here */
 
+const signIn = function() {
+    var unField = document.getElementById("username-field");
+    var pwField = document.getElementById("password-field");
+    var un = unField.value;
+    var pw = pwField.value;
+    var regexPass = (properRegex.test(un) && properRegex.test(pw));
+    var unCheck = (function () {/*Returns 'User' object at given index if entered username matches given User's name field, false if it matches to none.*/
+        for (var i = 0; i < userIndex.length; i++) {
+            if (userIndex[i].name === un) {
+                return userIndex[i];
+            } else {
+            }
+        }
+        return false;
+    })();
+
+    if (!regexPass) {
+        unField.value = "";
+        pwField.value = "";
+        window.alert("We cannot accept that username and/or password.\n\nPlease enter a string of letters ( [aA-zZ] and numbers [0-9], up to 20 characters long ) into both fields!");
+    }
+
+    else if (regexPass && !unCheck) {
+        window.alert("User does not exist! Please either Sign Up as that user, or try to log in with a different username.");
+    }
+
+    else if (regexPass && unCheck) {
+        var currentUser = unCheck;
+        console.log("Existing user found, pulled from localStorage: " + currentUser.name + " | " + currentUser.password + " .");
+        //window.location="symptoms.html"
+    }
+
+    else {
+        console.log("!!! CRITICAL: Unhandled 'if' case in function 'signIn()'. Investigate and repair!");
+    }
+};
+
+/* Cut out the middleman! *Anonymous* function to attach both listeners upon document.load here */
+
+window.addEventListener("load", function(){
+    document.getElementById("sign-in-button").addEventListener("click", signIn);
+    document.getElementById("sign-up-button").addEventListener("click", signUp);
+});
 
 /**
 
-To-Do: Diff all from/to localStorage, place proper definitions in massDefinitions.js, test proper creation etc, start coding nuts-and-bolts, including event handlers.
-
-Class User - *initialize*
-userIndex = localStorage.userIndex | [];
-Class Supplement - *initialize*
-supplementIndex = *populate with bulk code block, write to localStorage*
-symptomIndex = *definition, write to localStorage*
-currentUser = null;
-
- name
- link
- picture
- description
- price
- symptoms
- {
- "Energy" : int [effectiveness rating],
- "Digestion" : ,
- "Mood" : ,
- "Headaches" : ,
- "Foggy Brain" : ,
- "Bone Problems" : ,
- "Allergies" : ,
- "Sugar Cravings" : ,
- "Heart Trouble" : ,
- "Recurring Flu / Colds" : ,
- "Romantic Health" : ,
- "General Nutrition" :
- }
+ [//
+ User enters their information and selects a button. Blank fields returns alert.
+ 1a) Sign Up: Checks userIndex: Already-existing user returns alert, clears fields.
+ 1b) Sign Up: Checks userIndex: New user creates new user object with provided name and password, installs into currentUser object, saves to userIndex, and proceeds to 'symptoms' page.
+ 2a) Sign In: Checks userIndex: Existing username but incorrect password returns Alert.
+ 2b) Sign In: Checks userIndex: Existing username with correct password writes existing user in localStorage into currentUser object, proceeds to 'symptoms' page.
+ //] }
 
  **/
 
--- js/massDefinitions.js
+++ js/massDefinitions.js
@@ -1,3 +1,51 @@
-/**
- * Created by Main on 9/15/2017.
- */
+/* SYMPTOMS */
+
+const symptomIndex = [
+    ["Energy", "../images/symptoms/energy.png"],
+    ["Digestion", "../images/symptoms/digestion.png"],
+    ["Mood", "../images/symptoms/mood.png"],
+    ["Headaches", "../images/symptoms/headaches.png"],
+    ["Foggy Brain", "../images/symptoms/foggybrain.png"],
+    ["Bone Problems", "../images/symptoms/boneproblems.png"],
+    ["Allergies", "../images/symptoms/allergies.png"],
+    ["Sugar Cravings", "../images/symptoms/sugercravings.png"],
+    ["Heart Trouble", "../images/symptoms/hearttrouble.png"],
+    ["Recurring Flu / Colds", "../images/symptoms/recurringflu.png"],
+    ["Romantic Health", "../images/symptoms/romantichealth.png"],
+    ["General Nutrition", "../images/symptoms/generalnutrition.png"]
+];
+
+/* SUPPLEMENTS */
+
+function Supplement (inpObj) {
+    this.name = inpObj.name; //string
+    this.link = inpObj.link; //string - external link
+    this.picture = "./images/supplements/" + inpObj.name + ".jpg"; //image filename must equal supplement name
+    this.description = inpObj.description; //string. Good reason to make these in external blocks
+    this.price = inpObj.price;
+    this.symptoms = inpObj.symptoms;
+}
+
+var supplementIndex = [];
+
+const calciumComplex = new Supplement({
+    name : "Calcium Complex",
+    link : "",
+    description : "",
+    price: 29.95,
+    symptoms: {
+        "Energy" : 5,
+        "Digestion" : 5,
+        "Mood" : 2,
+        "Headaches" : 5,
+        "Foggy Brain" : 0,
+        "Bone Problems" : 3,
+        "Allergies" : 5,
+        "Sugar Cravings" : 7,
+        "Heart Trouble" : 8,
+        "Recurring Flu / Colds" : 4,
+        "Romantic Health" : 4,
+        "General Nutrition": 6
+    }
+});
+supplementIndex.push(calciumComplex);
